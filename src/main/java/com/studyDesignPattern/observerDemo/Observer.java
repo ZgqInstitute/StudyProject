452041d069d0e3a6e95aba1f75910b9bf3e50f75
ZGQQQQ <1731073069@qq.com>
2021-01-13 07:34:35 +0800

    观察者(监听)模式

--- src/main/java/com/studyDesignPattern/observerDemo/Observer.java
+++ src/main/java/com/studyDesignPattern/observerDemo/Observer.java
@@ -1,8 +1,11 @@
 package com.studyDesignPattern.observerDemo;
 
 /**
- * 观察者
+ * 观察者（这个接口相当于spring中的ApplicationListener<E extends ApplicationEvent>）
  */
 public interface Observer {
+    /**
+     * 该方法相当于 ApplicationListener 接口的onApplicationEvent()方法
+     */
     void update(Object object);
 }
--- src/main/java/com/studyDesignPattern/observerDemo/TestClass.java
+++ src/main/java/com/studyDesignPattern/observerDemo/TestClass.java
@@ -6,8 +6,14 @@ public class TestClass {
 	@Test
 	public void testMethod(){
 		Subject subject = new Subject();
-		subject.addObserver(new Observer01());
-		subject.addObserver(new Observer02());
+		//给主题对象添加观察者对象
+		Observer01 observer = new Observer01();
+		subject.addObserver(observer);
+		Observer02 observer1 = new Observer02();
+		subject.addObserver(observer1);
 		subject.notifyObserver("主题对象发生变化");
+
+		//从主题对象中移除观察者对象
+		subject.removeObserver(observer);
 	}
 }
