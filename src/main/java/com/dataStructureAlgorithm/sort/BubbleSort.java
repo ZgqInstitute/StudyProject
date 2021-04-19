025ee30379c49979ad8451f74a488d3c740b47eb
zhuguangquan <zhuguangquan@enmonster.com>
2021-04-20 05:53:52 +0800

    冒泡排序的时间复杂度

--- src/main/java/com/dataStructureAlgorithm/sort/BubbleSort.java
+++ src/main/java/com/dataStructureAlgorithm/sort/BubbleSort.java
@@ -10,7 +10,7 @@ public class BubbleSort {
 
 	@Test
 	public void test() {
-		int[] a = {7, 19, 5, 43, 8, 9};
+		int[] a = {6, 5, 4, 3, 2, 1};
 //		int[] a = new int[]{7, 2, 5, 1, 8, 9};
 
 		sort(a);
@@ -20,6 +20,18 @@ public class BubbleSort {
 		for (int i = 0; i < array.length - 1; i++) {
 			for (int j = 0; j < array.length - 1 - i; j++) {
 				int temp;
+				/* 在最坏情况下需要排序的数组为：[6, 5, 4, 3, 2, 1]
+				 * 6需要比较 6 - 1次 （n-1次）
+				 * 5需要比较 6 - 2次 （n-2次）
+				 * .....
+				 * 2需要比较 6 - 5次 （n-5次）
+				 *
+				 * 总共需要比较的次数：(n-1)+(n-2)+(n-3)+(n-4)+...+1 = n^2/2 - n/2
+				 *
+				 * 总共需要交换的次数：(n-1)+(n-2)+(n-3)+(n-4)+...+1 = n^2/2 - n/2
+				 *
+				 * 总执行次数（冒泡算法的时间复杂度）：n^2 - n  —> O(n^2)
+				 */
 				if (array[j] > array[j + 1]) {
 					temp = array[j];
 					array[j] = array[j + 1];
