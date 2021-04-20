3f84bf12a738b29477e6b4b1d975855016570698
zhuguangquan <zhuguangquan@enmonster.com>
2021-04-20 10:00:21 +0800

    插入排序

---
+++ src/main/java/com/dataStructureAlgorithm/sort/InsertionSort.java
@@ -0,0 +1,30 @@
+package com.dataStructureAlgorithm.sort;
+
+import org.junit.Test;
+
+/**
+ * 插入排序
+ *
+ */
+public class InsertionSort {
+	@Test
+	public void test() {
+		int[] a = {66, 5, 12, 4, 10, 6};//{6, 5, 4, 3, 2, 1}
+//		sort(a);
+	}
+
+	public void sort(int[] array){
+
+		for(int i = 0; i < array.length - 1; i++){
+			int minIndex = 1;
+			int min = array[i];
+			for(int j = i; j < array.length; j++){
+//				if(){
+//
+//				}
+			}
+
+		}
+	}
+
+}
