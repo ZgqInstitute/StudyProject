d95003c1324f46c7c17c68fbe6917be8a96c92b1
zhuguangquan <zhuguangquan@enmonster.com>
2021-04-21 19:33:49 +0800

    插入排序

--- src/main/java/com/dataStructureAlgorithm/sort/InsertionSort.java
+++ src/main/java/com/dataStructureAlgorithm/sort/InsertionSort.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
 public class InsertionSort {
 	@Test
 	public void test() {
-		int[] a = {5, 11, 2, 6, 10, 8};
+		int[] a = {5, 11, 2, 6, 10, 8};//最坏时间复杂度例子：{20,16,10,8,6,3}
 		// i = 1  {5, 11, 2, 6, 10, 8} —> {5, 11, 2, 6, 10, 8}
 		// i = 2  {5, 11, 2, 6, 10, 8} —> {5, 11, 11, 6, 10, 8} —> {5, 5, 11, 6, 10, 8} —> {2, 5, 11, 6, 10, 8}
 		sort(a);
@@ -20,15 +20,16 @@ public class InsertionSort {
 	public void sort(int[] array) {
 
 		for (int i = 1; i < array.length; i++) {
-			int insertVal = array[i];
-			int insertIndex = i -1;
+			int insertVal = array[i];//n
+			int insertIndex = i -1;//n
 
 			while (insertIndex >= 0 && insertVal < array[insertIndex]) {
+				//1  2  ... n     (1+n)*n = n^2 + n = n^2
 				array[insertIndex + 1] = array[insertIndex];
 				insertIndex -= 1;
 			}
 
-			array[insertIndex + 1] = insertVal;//
+			array[insertIndex + 1] = insertVal;//n
 		}
 
 		System.out.println(Arrays.toString(array));
