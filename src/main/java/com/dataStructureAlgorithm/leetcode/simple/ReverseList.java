ec9a912a9c0c42c4145f5ad5b8030f4f3e439d16
ZGQQQQ <1731073069@qq.com>
2021-05-07 15:31:39 +0800

    反转链表

--- src/main/java/com/dataStructureAlgorithm/leetcode/simple/ReverseList.java
+++ src/main/java/com/dataStructureAlgorithm/leetcode/simple/ReverseList.java
@@ -1,5 +1,7 @@
 package com.dataStructureAlgorithm.leetcode.simple;
 
+import org.junit.Test;
+
 /**
  * 需求：输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
  *  示例：
@@ -8,4 +10,32 @@ package com.dataStructureAlgorithm.leetcode.simple;
  */
 public class ReverseList {
 
+    @Test
+    public void test(){
+
+    }
+
+    public Node reverseList(Node head) {
+        Node prev = null;
+        Node curr = head;
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+
+    }
+
+    private class Node {
+        private Node next;
+        private int data;
+        private Node(Node head, int data){
+            this.next = head;
+            this.data = data;
+        }
+    }
 }
+
+
