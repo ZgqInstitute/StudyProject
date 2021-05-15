b40e6ef6c5ef57478aabc5e3d45e59b13a97fa6a
ZGQQQQ <1731073069@qq.com>
2021-05-15 20:17:37 +0800

    用链表模拟栈数据结构

---
+++ src/main/java/com/dataStructureAlgorithm/stackDemo/TestClass.java
@@ -0,0 +1,19 @@
+package com.dataStructureAlgorithm.stackDemo;
+
+import org.junit.Test;
+
+public class TestClass {
+
+    @Test
+    public void test(){
+        Stack<Integer> integerStack = new Stack<>();
+        integerStack.push(1);
+        integerStack.push(2);
+        integerStack.push(3);
+
+        System.out.println(integerStack.pop());
+        System.out.println(integerStack.pop());
+        System.out.println(integerStack.pop());
+
+    }
+}
