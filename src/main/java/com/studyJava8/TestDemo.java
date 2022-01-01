ed5a1b8bd98e22b476f811b04f77ddcfeedcf8dd
ZGQQQQ <gq.zhu@trip.com>
2022-01-01 10:23:05 +0800

    test

--- src/main/java/com/studyJava8/TestDemo.java
+++ src/main/java/com/studyJava8/TestDemo.java
@@ -2,20 +2,11 @@ package com.studyJava8;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.stream.Collectors;
 
 public class TestDemo {
 
 	@Test
 	public void testMethod() {
-		Person person = new Person();
-		ArrayList<Person> personList = new ArrayList<>();
-		personList.add(new Person("aaa"));
-		personList.add(new Person("bbb"));
-		personList.add(new Person("ccc"));
-		String collect = personList.stream().map(Person::getName).collect(Collectors.joining("--"));
-		person.setName(collect);
-		System.out.println(person.getName());
+
 	}
 }
