85b70261498b87258c4382988e75a8b979950942
gq_zhu <zhuguangquan0417@163.com>
2022-07-20 13:40:07 +0800

    选择排序修改

--- src/main/java/com/studyAlgorithm/sort/simpleSort/SelectSort.java
+++ src/main/java/com/studyAlgorithm/sort/simpleSort/SelectSort.java
@@ -14,39 +14,19 @@ public class SelectSort {
 
 	@Test
 	public void test() {
-		int[] a = {66, 5, 12, 4, 10, 6};//{6, 5, 4, 3, 2, 1}
+		int[] a = {7, 5, 12, 4, 10, 6, 3};//{6, 5, 4, 3, 2, 1}
 		sort(a);
 	}
 
-	public void sort(int[] array) {
-
+	private void sort(int[] array) {
 		for (int i = 0; i < array.length - 1; i++) {
-			int minIndex = i;
-			int min = array[i];
-
 			for (int j = i + 1; j < array.length; j++) {
-
-				/* 时间复杂度
-				 * 假如需要排序的队列为：{6, 5, 4, 3, 2, 1}
-				 * 6需要比较 6 - 1次 （n-1次）
-				 * 5需要比较 6 - 2次 （n-2次）
-				 * .....
-				 * 2需要比较 6 - 5次 （n-5次）
-				 *
-				 * 总共需要比较的次数：(n-1)+(n-2)+(n-3)+(n-4)+...+1 = n^2/2 - n/2
-				 *
-				 * 总共需要交换的次数：n -1
-				 *
-				 * 总执行次数（排序算法的时间复杂度）： O(n^2)
-				 */
-				if (min > array[j]) {
-					min = array[j];
-					minIndex = j;
+				int temp;
+				if (array[i] > array[j]) {
+					temp = array[i];
+					array[i] = array[j];
+					array[j] = temp;
 				}
-			}
-			if (minIndex != i) {
-				array[minIndex] = array[i];
-				array[i] = min;
 			}
 		}
 
